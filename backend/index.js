b7dca18caf28ec01a8bd2416a21ec014794646f1
maliki <devossama9@gmail.com>
2026-02-08 17:00:53 +0100

    fix add media uploading path for prod

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,7 @@ app.use(cors({
 // General Middleware
 // ============================================================================
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Body parsing
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
