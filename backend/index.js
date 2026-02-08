4ec2b97da3b550321abdf99a512762be540507db
maliki <devossama9@gmail.com>
2026-02-08 17:14:02 +0100

    fix ES module issues

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,10 @@ import logger from './utils/logger.js';
 import { disconnectPrisma } from './utils/prisma.js';
 
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
